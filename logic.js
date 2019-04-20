b9790c7933621e7d463aac3da4f705c9b4d6ed77
unknown <sluground@gmail.com>
2019-04-20 11:16:42 -0400

    phase 3

--- logic.js
+++ logic.js
@@ -12,22 +12,36 @@ var database = firebase.database();
       role = $("#role-input").val().trim();
       startDate = $("#startDate-input").val().trim();
       rate = $("#rate-input").val().trim();
-      database.ref().set({
+      
+      // New Push Code
+      database.ref().push({
         name: name,
         role: role,
         startDate: startDate,
-        rate: rate
+        rate: rate,
+        dateAdded: firebase.database.servervalue.TIMESTAMP
       });
     });
     // Firebase watcher + initial loader HINT: .on("value")
-    database.ref().on("value", function(snapshot) {
+    database.ref().on("value", function(childSnapshot) {
       // Log everything that's coming out of snapshot
-      console.log(snapshot.val());
-      console.log(snapshot.val().name);
-      console.log(snapshot.val().role);
-      console.log(snapshot.val().startDate);
-      console.log(snapshot.val().rate);
+      console.log(childSnapshot.val());
+      console.log(childSnapshot.val().name);
+      console.log(childSnapshot.val().role);
+      console.log(childSnapshot.val().startDate);
+      console.log(childSnapshot.val().rate);
+    });
+    $("#full-member-list").append("<tr class='well'><td class='member-name'> " +
+        childSnapshot.val().name +
+        " </td><td class='member-role'> " + childSnapshot.val().role +
+        " </td><td class='member-startDate'> " + childSnapshot.val().startDate +
+        " </td><td class='member-rate'> " + childSnapshot.val().rate +
+        " </td></tr");
+
+  // Handle the errors
+
       // Change the HTML to reflect
+      dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
       $("#name-display").text(snapshot.val().name);
       $("#role-display").text(snapshot.val().role);
       $("#startDate-display").text(snapshot.val().startDate);
