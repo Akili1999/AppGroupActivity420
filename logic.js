e7c91725f06afe2e8929352073af9a4771bbb084
unknown <sluground@gmail.com>
2019-04-20 10:20:10 -0400

    updated logic

--- logic.js
+++ logic.js
@@ -1,50 +1,37 @@
 firebase.initializeApp(config);
 var database = firebase.database();
-
     // Initial Values
     var name = "";
-    var email = "";
-    var age = 0;
-    var comment = "";
-
+    var role = "";
+    var startDate = "";
+    var rate = "";
     // Capture Button Click
     $("#add-user").on("click", function(event) {
-      // Don't refresh the page!
       event.preventDefault();
-
-      // YOUR TASK!!!
-      // Code in the logic for storing and retrieving the most recent user.
-      // Don't forget to provide initial data to your Firebase database.
       name = $("#name-input").val().trim();
-      email = $("#email-input").val().trim();
-      age = $("#age-input").val().trim();
-      comment = $("#comment-input").val().trim();
-
+      role = $("#role-input").val().trim();
+      startDate = $("#startDate-input").val().trim();
+      rate = $("#rate-input").val().trim();
       database.ref().set({
         name: name,
-        email: email,
-        age: age,
-        comment: comment
+        role: role,
+        startDate: startDate,
+        rate: rate
       });
-
     });
-
     // Firebase watcher + initial loader HINT: .on("value")
     database.ref().on("value", function(snapshot) {
-
       // Log everything that's coming out of snapshot
       console.log(snapshot.val());
       console.log(snapshot.val().name);
-      console.log(snapshot.val().email);
-      console.log(snapshot.val().age);
-      console.log(snapshot.val().comment);
-
+      console.log(snapshot.val().role);
+      console.log(snapshot.val().startDate);
+      console.log(snapshot.val().rate);
       // Change the HTML to reflect
       $("#name-display").text(snapshot.val().name);
-      $("#email-display").text(snapshot.val().email);
-      $("#age-display").text(snapshot.val().age);
-      $("#comment-display").text(snapshot.val().comment);
-
+      $("#role-display").text(snapshot.val().role);
+      $("#startDate-display").text(snapshot.val().startDate);
+      $("#rate-display").text(snapshot.val().rate);
       // Handle the errors
     }, function(errorObject) {
       console.log("Errors handled: " + errorObject.code);
